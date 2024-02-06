d49bccfc5d826c524999e0756d416011b76b2d77
Mobin Fazli <m0binfae2003@gmail.com>
2024-02-05 20:48:52 -0500

    Simple Stopwatch

--- script.js
+++ script.js
@@ -1,33 +1,48 @@
 window.onload = function() {
 
+    // DOM elements for time display and watch buttons!
     var seconds  = document.querySelector("#sec");
     var minutes  = document.querySelector("#min");
     var hours    = document.querySelector("#hour");
+
     var startBtn = document.querySelector("#start");
     var stopBtn  = document.querySelector("#stop");
     var resetBtn = document.querySelector("#reset");
 
+    // Variable to store the interval ID for time updates!
     var interval;
 
+    // Initialize variables for tracking time values!
     var secondsValue = 0;
     var minutesValue = 0;
     var hourValue    = 0;
 
+    // Initialize displayed time values!
     seconds.innerHTML = ":00";
     minutes.innerHTML = ":00";
     hours.innerHTML   = "00";
 
+    // Event handler for the start button!
     startBtn.onclick = function() {
+        // Clear any existing interval!
         clearInterval(interval);
+
+        // Start updating time every second!
         interval = setInterval(start, 1000);
     }
 
+    // Event handler for the stop button!
     stopBtn.onclick = function() {
+        // Stop the time update interval!
         clearInterval(interval);
     }
 
+    // Event handler for the reset button!
     resetBtn.onclick = function() {
+        // Stop the time update interval!
         clearInterval(interval);
+
+        // Reset time values to zero!
         secondsValue = 0;
         minutesValue = 0;
         hourValue    = 0;
@@ -37,15 +52,18 @@ window.onload = function() {
         hours.innerHTML   = "00";
     }
 
+    // Function to update the time values!
     function start() {
         secondsValue++;
 
+          // Update seconds display!
         if (secondsValue < 10) {
             seconds.innerHTML = ":0" + secondsValue;
         } else {
             seconds.innerHTML = ":" + secondsValue;
         }
 
+         // Update minutes display!
         if (secondsValue > 59) {
             minutesValue++;
             secondsValue = 0;
@@ -57,6 +75,7 @@ window.onload = function() {
             }
         }
 
+        // Update hours display!
         if (minutesValue > 59) {
             hourValue++;
             minutesValue = 0;
